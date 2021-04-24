d6970887a0671f195fc3f1bae063a3c30d49fa48
stacy <stacydn23@gmail.com>
2021-04-24 17:40:22 -0400

    build the post controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,33 @@
+class PostsController < ApplicationController
+
+    def new
+        @post = Post.new
+    end
+
+    def index
+        @posts = current_user.posts
+    end 
+
+    def create
+        @post = Post.new(post_params)
+        @post.user = current_user
+        if @post.save
+            redirect_to post_path
+        else
+            render :new
+        end
+    end
+
+    def show
+        @posts = Post.find_by_id(params[:id])
+    end
+
+    def update
+    end
+
+    private
+
+    def post_params 
+        params.require(:post).permit(:title, :content, :user_id)
+    end
+end
