1ad9ad3481ae8002a3d324e032f1c18874c93fac
stacy <stacydn23@gmail.com>
2021-04-24 15:54:54 -0400

    structure home page, as well as build signup and login routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    helper_method :current_user, :logged_in?
+
+    # return current user if any
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def authorize
+        if current_user.nil?
+            flash[:error] = "You must be logged in to access this page."
+            redirect_to login_path
+        end
+    end
+
+    def logged_in?
+        !!current_user # returns a boolean true/false if user is logged in 
+    end
+
+    def require_logged_in
+        if current_user.nil?
+            redirect_to login_path
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Homepage 
+  root 'welcome#home'
+
+  # signup route
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+
+  # login route
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+
+
 end
