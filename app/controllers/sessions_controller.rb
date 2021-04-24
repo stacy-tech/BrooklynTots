c0b65554e3857e1242f7ca7d29cadb7006d7ba14
stacy <stacydn23@gmail.com>
2021-04-24 17:03:33 -0400

    create omniauth method as well as logout method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,5 +13,26 @@ class SessionsController < ApplicationController
         end
     end
 
+    def omniauth
+        @user = User.find_or_create_by(uid: auth['uid']) do |u|
+            u.name = auth['info']['name']
+            u.email = auth['info']['email']
+            u.password = SecureRandom.hex
+        end
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+    end 
+
+    def destroy
+        session.clear
+        redirect_to root_path
+    end
+
+    private
+
+    def auth
+        request.env['omniauth.auth']
+    end
+
     
 end
