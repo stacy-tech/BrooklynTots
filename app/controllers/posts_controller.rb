b0b33d454940cbb13ab444840829c1cde641f2a8
stacy <stacydn23@gmail.com>
2021-04-26 11:50:27 -0400

    create a delete method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,7 @@ class PostsController < ApplicationController
         @post = Post.new(post_params)
         @post.user = current_user
         if @post.save
-            redirect_to posts_path(@post)
+            redirect_to post_path(@post)
         else
             render :new
         end
@@ -41,15 +41,9 @@ class PostsController < ApplicationController
     end
 
     def destroy
-        post = Post.find(params[:id])
-        if current_user == post.user
-            post.destroy
-            flash[:sucess]="Successfully deleted."
-        else
-            flash[:error]= workout.errors.full_messages.to_sentence
-        end
+        @post = Post.find(params[:id])
+        @post.destroy
         redirect_to posts_path
-
     end
 
     private
